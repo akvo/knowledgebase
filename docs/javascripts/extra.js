673492df6b9bdaca5cb81ee97bc55674b2ce44c8
Jonah Kisioh <jkisioh@gmail.com>
2023-01-19 15:11:35 +0300

    Change config to include extra.js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -5,12 +5,4 @@ var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("
 var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
 tags.async=!0,tags.src="https://analytics.akvo.org/containers/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts);
 !function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]);
-})(window, document, 'dataLayer', '{{ config.piwik }}');
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', '{{ config.google_analytics[0] }}', '{{ config.google_analytics[1] }}');
-          ga('send', 'pageview');
+})(window, document, 'dataLayer', '6ff13965-650f-49d6-8a20-a2c7b7c4c4d9');
